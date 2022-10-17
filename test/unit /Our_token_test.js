bc730a4d0b5ae19078924a4db0543347efa4319a
kihiuFrank <franklinekihiu@gmail.com>
2022-10-17 18:22:28 +0300

    deployed, token name & symbol tests

--- test/unit /Our_token_test.js
+++ test/unit /Our_token_test.js
@@ -1,5 +1,5 @@
+const { assert } = require("chai")
 const { getNamedAccounts, deployments, network, ethers } = require("hardhat")
-const { beforeEach } = require("mocha")
 const {
   developmentChains,
   INITIAL_SUPPLY,
@@ -10,11 +10,30 @@ const {
   : describe("Our Token Unit Tests", () => {
       let ourToken, deployer, user1
       beforeEach(async () => {
-        const accounts = ethers.getNamedAccounts
+        const accounts = await getNamedAccounts()
         deployer = accounts.deployer
         user1 = accounts.user1
 
         await deployments.fixture(["all"])
         ourToken = await ethers.getContract("OurToken", deployer)
+      })
+
+      it("was deployed", async () => {
+        assert(ourToken.address)
+      })
+
+      describe("constructor", () => {
+        it("has correct initial supply of tokens", async () => {
+          const totalSupply = await ourToken.totalSupply()
+          assert.equal(totalSupply.toString(), INITIAL_SUPPLY)
+        })
+
+        it("initializes the token name and the symbol correctly", async () => {
+          const tokenName = (await ourToken.name()).toString()
+          assert.equal(tokenName, "OurToken")
+
+          const tokenSymbol = (await ourToken.symbol()).toString()
+          assert.equal(tokenSymbol, "OT")
+        })
       })
     })
